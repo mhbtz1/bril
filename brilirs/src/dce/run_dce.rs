273891c13c023a20d4c64f6477330d67879324a5
Matthew Habtezgi <mhbtz@mit.edu>
2024-12-24 23:50:28 -0500

    use basic block semantics instead

--- brilirs/src/dce/run_dce.rs
+++ brilirs/src/dce/run_dce.rs
@@ -1,57 +1,55 @@
 use std::collections::{HashSet, HashMap};
 use bril_rs::{Program, Function, Code, Instruction};
+use crate::{BBProgram, BBFunction};
 
-
-pub fn full_dce(program: Program) -> Program {
+pub fn full_dce(program: BBProgram) -> BBProgram {
     let mut new_program = program.clone();
-    new_program.functions.clear();
-    for func in program.functions {
+    new_program.func_index.clear();
+    for func in program.func_index{
         let upd_func = run_dce_func(&func);
-        new_program.functions.push(upd_func);
+        new_program.func_index.push(upd_func);
     }
     new_program
 }
 
-pub fn run_dce_func(block: &Function) -> Function {
+pub fn run_dce_func(block: &BBFunction) -> BBFunction {
     // currently only does one pass of DCE, so not fully done
 
-    let mut new_func = Function {
-        name: block.name.clone(),
-        args: block.args.clone(),
-        instrs: Vec::new(),
-        pos: block.pos.clone(),
-        return_type: block.return_type.clone()
-    };
+    let mut new_func = block.clone();
+    block.blocks.into_iter().map(|x| x.instrs.clear());
     
     let mut used = HashSet::new();
     let mut map: HashMap<String, usize> = HashMap::new();
-    let tup_iters = block.instrs.clone().into_iter().enumerate().collect::<Vec<(usize, Code)>>();   
-    for (line, instr) in &tup_iters{
-        if let Code::Instruction(Instruction::Value { args, dest, funcs: _, labels: _,  op: _, pos: _, op_type: _}) = instr {
-            map.insert(dest.clone().to_string(), *line);
-        }
-    }
-
-    for (line, instr) in &tup_iters {
-        match instr {
-            Code::Instruction(Instruction::Value { args, dest, funcs: _, labels: _,  op: _, pos: _, op_type: _}) => {
-                used.insert(dest);
+    
+    for blk in &block.blocks {
+        let tup_iters = blk.instrs.clone().into_iter().enumerate().collect::<Vec<_>>();   
+        for (line, instr) in tup_iters {
+            if let Instruction::Value { args, dest, funcs: _, labels: _,  op: _, pos: _, op_type: _} = instr {
+               map.insert(dest.clone().to_string(), *line);
             }
-            _ => ()
         }
-    }
-        
-    for (line, instr) in &tup_iters {
-        match instr {
-            Code::Instruction(Instruction::Value { args, dest, funcs: _, labels: _,  op: _, pos: _, op_type: _}) => {
-                if let Some(&line) = map.get(dest) {
-                    if used.contains(&dest) {
-                        new_func.instrs.push(instr.clone());
-                    }
+
+        for (line, instr) in &tup_iters {
+            match instr {
+                Instruction::Value { args, dest, funcs: _, labels: _,  op: _, pos: _, op_type: _} => {
+                    used.insert(dest);
                 }
-
+                _ => ()
             }
-            _ => ()
+        }
+        
+        for (line, instr) in &tup_iters {
+            match instr {
+                Instruction::Value { args, dest, funcs: _, labels: _,  op: _, pos: _, op_type: _} => {
+                    if let Some(&line) = map.get(dest) {
+                        if used.contains(&dest) {
+                            new_func.instrs.push(instr.clone());
+                        }
+                    }
+
+                }
+                _ => ()
+            }
         }
     }
     new_func
